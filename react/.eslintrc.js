fb5c589d765de9cad3b55ce3d20096219268c1e8
ncor <toffuyume@gmail.com>
2024-06-22 03:50:01 +0300

    feat: init package

---
+++ react/.eslintrc.js
@@ -0,0 +1,9 @@
+/** @type{ import("eslint").Linter.BaseConfig } */
+module.exports = {
+    extends: [
+        "eslint-plugin-react/configs/all"
+    ],
+    rules: {
+        "unicorn/prefer-module": 2
+    }
+};
